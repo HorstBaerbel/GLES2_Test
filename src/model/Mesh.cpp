99bc745f638efd137b584896ae8a63bd4f5ab1e4
Bim <bim.overbohm@googlemail.com>
2013-08-05 01:17:45 +0200

    Add mesh and model files

--- src/model/Mesh.cpp
+++ src/model/Mesh.cpp
@@ -11,36 +11,33 @@ void Mesh::addVertexBuffer(std::shared_ptr<GLVertexBuffer> & buffer)
 	vertexBuffers.push_back(buffer);
 }
 
-bool Mesh::prepareRender(std::shared_ptr<ParameterBase> parameter) override
+bool Mesh::prepareRender(std::shared_ptr<ParameterBase> parameter)
 {
 	bool result = true;
-	std::vector<std::shared_ptr<GLVertexBuffer>> vbIt = vertexBuffers.cbegin();
-	while (vbIt != vertexBuffers.cend()) {
-		if (!vbIt->prepareRender(parameter)) {
+	for (auto vbIt = vertexBuffers.begin(); vbIt != vertexBuffers.cend(); ++vbIt) {
+		if (!(*vbIt)->prepareRender(parameter)) {
 			result = false;
 		}
 	}
 	return result;
 }
 
-bool Mesh::render(std::shared_ptr<ParameterBase> parameter) override
+bool Mesh::render(std::shared_ptr<ParameterBase> parameter)
 {
 	bool result = true;
-	std::vector<std::shared_ptr<GLVertexBuffer>> vbIt = vertexBuffers.cbegin();
-	while (vbIt != vertexBuffers.cend()) {
-		if (!vbIt->render(parameter)) {
+	for (auto vbIt = vertexBuffers.begin(); vbIt != vertexBuffers.cend(); ++vbIt) {
+		if (!(*vbIt)->render(parameter)) {
 			result = false;
 		}
 	}
 	return result;
 }
 
-bool Mesh::finishRender(std::shared_ptr<ParameterBase> parameter) override
+bool Mesh::finishRender(std::shared_ptr<ParameterBase> parameter)
 {
-	bool result = true;
-	std::vector<std::shared_ptr<GLVertexBuffer>> vbIt = vertexBuffers.cbegin();
-	while (vbIt != vertexBuffers.cend()) {
-		if (!vbIt->finishRender(parameter)) {
+	bool result = true;	
+	for (auto vbIt = vertexBuffers.begin(); vbIt != vertexBuffers.cend(); ++vbIt) {
+		if (!(*vbIt)->finishRender(parameter)) {
 			result = false;
 		}
 	}
